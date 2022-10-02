6712b8528e4cc7431e8ed044a8aa0d819733a592
AbhishekSrivastava1999 <87290849+AbhishekSrivastava1999@users.noreply.github.com>
2022-10-02 03:11:40 -0400

    Update variables.tf

--- terraform_code/dev/instances/variables.tf
+++ terraform_code/dev/instances/variables.tf
@@ -12,7 +12,7 @@ variable "instance_type" {
 
 # Variable to signal the current environment 
 variable "env" {
-  default     = "env_dev"
+  default     = "dev"
   type        = string
   description = "Deployment Environment"
 }
